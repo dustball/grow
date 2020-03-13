5b458bfbdc2817dbf3427782bff303430463483c
Brian Klug <brian.klug@gmail.com>
2020-03-13 18:06:57 -0500

    debugging code

--- ecowitt.py
+++ ecowitt.py
@@ -10,7 +10,7 @@ print ('Content-Type: text/html\n\n')
 arguments = cgi.FieldStorage()
 
 if 2+2==5:
-    f= open("out.txt","a+")
+    f= open("debug.txt","a+")
     for i in arguments.keys():
         f.write(i+"="+arguments[i].value+"\n")
     f.close() 
@@ -31,25 +31,3 @@ for i in range(1,8+1):
         mydb.commit()
     
 print ("200 OK")
-
-#soilbatt2=1.7
-#dateutc=2020-03-09 15:50:49
-#baromabsin=29.137
-#soilmoisture6=27
-#tempinf=76.6
-#soilbatt5=1.8
-#soilmoisture4=30
-#soilbatt3=1.8
-#model=GW1000_Pro
-#soilmoisture5=36
-#soilmoisture1=40
-#freq=915M
-#stationtype=GW1000B_V1.5.6
-#soilmoisture2=25
-#soilbatt6=1.7
-#baromrelin=29.137
-#PASSKEY=xxxx
-#soilbatt1=1.7
-#soilmoisture3=26
-#soilbatt4=1.8
-#humidityin=62
