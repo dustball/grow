9c19d0b8036b832367bd30d3dec84939d8ecf797
Brian Klug <brian.klug@gmail.com>
2020-04-27 13:27:02 -0500

    fix image fetch

--- get-image.bash
+++ get-image.bash
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+pkill -9 wget
+
 set -e
 
-wget "http://view:view1234@192.168.0.43:80/cgi-bin/snapshot.cgi" -O image-dl.jpg
+wget --timeout 8 -r --tries=5 -w 2 --retry-connrefused "http://view:view1234@192.168.0.43:80/cgi-bin/snapshot.cgi" -O image-dl.jpg
 
 cp image-dl.jpg image.jpg
